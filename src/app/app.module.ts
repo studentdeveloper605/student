30913896e65b3730372c9d10738325422dae5b32
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-05-01 19:19:48 +0530

    Chat Screen Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomBarComponent
   ],
   imports: [
     BrowserModule
